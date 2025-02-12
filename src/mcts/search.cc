f6cfe06c7a1d67066a4d159c68e0271c3f051972
Daniel Monroe <github.com@danielmonroe.net>
2025-02-11 16:34:42 -0800

    1.5x policy on unvisited checks

--- src/mcts/search.cc
+++ src/mcts/search.cc
@@ -1934,11 +1934,13 @@ void SearchWorker::PickNodesToExtendTask(
           }
           float weightstarted = current_weightstarted[idx];
           const float util = current_util[idx];
+          
           if (idx > cache_filled_idx) {
             float p = cur_iters[idx].GetP();
+            bool check = cur_iters[idx].GetCheck();
 
             // a small hack to reduce policy on bad moves
-            if (p < 0.01f) p /= 3;
+            if (p < 0.01f && !check) p /= 3;
             //if (cur_iters[idx].GetWL(0.0f) < -0.995) p /= 5;
             //else if (cur_iters[idx].GetWL(0.0f) < -0.99) p /= 3;
             //else if (cur_iters[idx].GetWL(0.0f) < -0.95) p /= 2;
@@ -1953,7 +1955,9 @@ void SearchWorker::PickNodesToExtendTask(
               if (util >= min_policy_boost_util_t2) {
                 p = std::max(p, policy_boost_t2);
               }
-            }
+              else if (check && p > 0.01f && p < 0.1f) {
+                p = std::min(p * 1.5f, 0.1f);
+              }
 
             
             current_score[idx] =
