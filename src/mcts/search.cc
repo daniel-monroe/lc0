e9fd1f774588dbe2afd127ff6a974f9313e4b627
Daniel Monroe <github.com@danielmonroe.net>
2025-02-28 16:31:32 -0800

    idk how to code

--- src/mcts/search.cc
+++ src/mcts/search.cc
@@ -573,7 +573,7 @@ inline float ComputeWeight(const SearchParams& params, float uncertainty) {
   const float cap = params.GetUncertaintyWeightingCap();
   const float coefficient = params.GetUncertaintyWeightingCoefficient();
   const float exponent = params.GetUncertaintyWeightingExponent();
-  return fmin(cap, coefficient * pow(uncertainty, exponent)  /
+  return fmin(cap, coefficient * pow(uncertainty, exponent))  /
          (1 + (uncertainty > 0.35f));
 }
 
