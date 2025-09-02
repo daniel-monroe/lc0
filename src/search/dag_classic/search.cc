044899cfc4466d853308b313dae6081f1f511f61
Daniel Monroe <github.com@danielmonroe.net>
2025-09-02 11:32:20 -0400

    a

--- src/search/dag_classic/search.cc
+++ src/search/dag_classic/search.cc
@@ -1743,8 +1743,9 @@ void SearchWorker::PickNodesToExtendTask(
           int nstarted = current_nstarted[idx];
           const float util = current_util[idx];
           if (idx > cache_filled_idx) {
+            bool awful_move = cur_iters[idx].GetP() < 0.01 && util < -0.98;
             current_score[idx] =
-                cur_iters[idx].GetP() * puct_mult / (1 + nstarted) + util;
+                cur_iters[idx].GetP() * puct_mult / (1 + nstarted) / (awful_move ? 4 : 1) + util;
             cache_filled_idx++;
           }
           if (is_root_node) {
